281ef5d8143cda458f1678068fc3b65d797db679
tomasqagz <tomas.quiroga@sarsy-srl.com>
2025-01-23 22:16:48 -0300

    added new option Ñ uppercase

--- README.md
+++ README.md
@@ -3,10 +3,9 @@ Simple program made in Golang that allows you to copy the "ñ" letter.
 
 # installation
 
-1) go build -ldflags="-H=windowsgui" -o mi-enie.exe // to make the executable
+1) go build -ldflags="-H=windowsgui" -o mi-enie.exe // to make the .exe is if not exist.
 2) take the .exe to a folder, example C:\Servicios\enie.exe
-2) Start-Process -FilePath "C:\Servicios\enie.exe" -WindowStyle Hidden //run on startup
-
+3) Start-Process -FilePath "C:\Servicios\enie.exe" -WindowStyle Hidden //run on startup
 
 
 
--- main.go
+++ main.go
@@ -12,9 +12,14 @@ func onExit() {
 	log.Println("Exiting...")
 }
 
-func copyLetter() {
+func copyLetter(caps bool) {
 
-	err := clipboard.WriteAll("ñ")
+	var letter = "ñ"
+	if caps {
+		letter = "Ñ"
+	}
+
+	err := clipboard.WriteAll(letter)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -28,14 +33,17 @@ func main() {
 		systray.SetTitle("Copiar Ñ")
 		systray.SetTooltip("Haz clic para copiar 'ñ'")
 
-		mCopy := systray.AddMenuItem("Copiar Ñ", "Copia la letra ñ al portapapeles")
+		mCopy := systray.AddMenuItem("Copiar ñ", "Copia la letra ñ al portapapeles")
+		mCopy2 := systray.AddMenuItem("Copiar Ñ", "Copia la letra Ñ al portapapeles")
 		mQuit := systray.AddMenuItem("Salir", "Salir de la aplicación")
 
 		go func() {
 			for {
 				select {
 				case <-mCopy.ClickedCh:
-					copyLetter()
+					copyLetter(false)
+				case <-mCopy2.ClickedCh:
+					copyLetter(true)
 				case <-mQuit.ClickedCh:
 					systray.Quit()
 					return
